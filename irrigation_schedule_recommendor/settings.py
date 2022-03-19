344ae2419f21d3b845a27a33a110064391dfd34c
Paras <soodparassood4@gmail.com>
2022-03-19 23:09:45 +0530

    Minor deployment settings changed

--- irrigation_schedule_recommendor/settings.py
+++ irrigation_schedule_recommendor/settings.py
@@ -11,9 +11,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 AUTH_USER_MODEL ='backend.User'
 # Quick-start development settings - unsuitable for production
